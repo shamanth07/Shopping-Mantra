0aac54c3fa251d38a1b2008caf12a2c4b693b02f
Shamanth Reddy <shamanthanireddy07@gmail.com>
2024-10-30 02:01:13 -0400

    first commit

---
+++ shoppingcartapp/app/src/main/java/com/example/shoppingcartapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.shoppingcartapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_sign_out) {
+            auth.signOut();
+            startActivity(new Intent(MainActivity.this, SignInActivity.class));
+            finish();
+            Toast.makeText(this, "Signed Out", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
