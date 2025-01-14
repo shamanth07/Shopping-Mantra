376e2b68177fba090ca87f5d55442f44db550ae1
shamanth07 <shamanthanireddy07@gmail.com>
2025-01-14 13:46:02 -0500

    added prevalent class

---
+++ app/src/main/java/com/example/shoppingmantra/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.shoppingmantra.Prevalent;
+
+import com.example.shoppingmantra.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+    public static String currentOnlineUserEmail;
+
+    public static final String UserEmailKey = "UserEmail";
+    public static final String UserPasswordKey = "UserPassword";
+}
