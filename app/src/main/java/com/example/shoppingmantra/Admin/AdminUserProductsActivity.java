13d436723dc37d08cf9c572663cf4f6949e4596f
shamanth07 <shamanthanireddy07@gmail.com>
2025-01-15 23:09:15 -0500

    updated admin orders

--- app/src/main/java/com/example/shoppingmantra/Admin/AdminUserProductsActivity.java
+++ app/src/main/java/com/example/shoppingmantra/Admin/AdminUserProductsActivity.java
@@ -1,6 +1,5 @@
 package com.example.shoppingmantra.Admin;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,68 +10,74 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.shoppingmantra.Model.Products;
-import com.example.shoppingmantra.R;
-import com.example.shoppingmantra.ViewHolder.ProductViewHolder;
+import com.example.ecommerceapp.Model.Cart;
+import com.example.ecommerceapp.R;
+import com.example.ecommerceapp.ViewHolder.CartViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.squareup.picasso.Picasso;
 
-public class AdminUserProductsActivity extends AppCompatActivity {
-
+public class AdminUserProductsActivity extends AppCompatActivity
+{
     private RecyclerView productsList;
-    private DatabaseReference productsRef;
+    RecyclerView.LayoutManager layoutManager;
+    private DatabaseReference cartListRef;
+
+    private String userID = "";
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_user_products);
 
+
+        userID = getIntent().getStringExtra("uid");
+
+
         productsList = findViewById(R.id.products_list);
         productsList.setHasFixedSize(true);
-        productsList.setLayoutManager(new LinearLayoutManager(this));
+        layoutManager = new LinearLayoutManager(this);
+        productsList.setLayoutManager(layoutManager);
 
-        // Reference to the Products node in Firebase
-        productsRef = FirebaseDatabase.getInstance().getReference().child("Products");
+
+        cartListRef = FirebaseDatabase.getInstance().getReference()
+                .child("Cart List").child("Admin View").child(userID).child("Products");
     }
 
+
     @Override
-    protected void onStart() {
+    protected void onStart()
+    {
         super.onStart();
 
-        FirebaseRecyclerOptions<Products> options =
-                new FirebaseRecyclerOptions.Builder<Products>()
-                        .setQuery(productsRef, Products.class)
-                        .build();
 
-        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter =
-                new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
-                    @Override
-                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull Products model) {
-                        holder.txtProductName.setText("Name: " + model.getPname());
-                        holder.txtProductPrice.setText("Price: $" + model.getPrice());
-                        holder.txtProductDescription.setText("Description: " + model.getDescription());
-                        Picasso.get().load(model.getImage()).into(holder.imageView);
-                        // Set onClickListener to navigate to AdminMaintainProductsActivity
-                        holder.itemView.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Intent intent = new Intent(AdminUserProductsActivity.this, AdminMaintainProductsActivity.class);
-                                intent.putExtra("pid", model.getPid()); // Pass product ID
-                                startActivity(intent);
-                            }
-                        });
-                    }
+        FirebaseRecyclerOptions<Cart> options =
+                new FirebaseRecyclerOptions.Builder<Cart>()
+                .setQuery(cartListRef, Cart.class)
+                .build();
 
-                    @NonNull
-                    @Override
-                    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout, parent, false);
-                        return new ProductViewHolder(view);
-                    }
-                };
+        FirebaseRecyclerAdapter<Cart, CartViewHolder> adapter = new FirebaseRecyclerAdapter<Cart, CartViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull CartViewHolder holder, int position, @NonNull Cart model)
+            {
+                holder.txtProductQuantity.setText("Quantity = " + model.getQuantity());
+                holder.txtProductPrice.setText("Price " + model.getPrice() + "$");
+                holder.txtProductName.setText(model.getPname());
+            }
+
+            @NonNull
+            @Override
+            public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+            {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_items_layout, parent, false);
+                CartViewHolder holder = new CartViewHolder(view);
+                return holder;
+            }
+        };
 
         productsList.setAdapter(adapter);
         adapter.startListening();
