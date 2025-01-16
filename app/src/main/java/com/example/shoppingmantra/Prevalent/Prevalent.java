c8d4362097837bea34aff490456e604ddae4c9ba
shamanth07 <shamanthanireddy07@gmail.com>
2025-01-15 23:11:57 -0500

    updated product details

--- app/src/main/java/com/example/shoppingmantra/Prevalent/Prevalent.java
+++ app/src/main/java/com/example/shoppingmantra/Prevalent/Prevalent.java
@@ -1,6 +1,6 @@
 package com.example.shoppingmantra.Prevalent;
 
-import com.example.shoppingmantra.Model.Users;
+import com.example.ecommerceapp.Model.Users;
 
 public class Prevalent {
     public static Users currentOnlineUser;
